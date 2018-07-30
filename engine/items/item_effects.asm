3d109daa04e09ddf6db1c44d926b4a53a8c62540
toomanyluigis <toomanyluigis11@gmail.com>
2018-07-30 11:28:06 +0100

    NEW Staryu/Starmie, corrected Mareep back,moon ball and catch rate fix

--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -361,12 +361,13 @@ PokeBallEffect:
 ; no status effect at all. But instead, it makes BRN/PSN/PAR provide no
 ; benefit.
 ; Uncomment the line below to fix this.
+; UPDATE: Bug has been fixed.
 	ld b, a
 	ld a, [wEnemyMonStatus]
 	and 1 << FRZ | SLP
 	ld c, 10
 	jr nz, .addstatus
-	; ld a, [wEnemyMonStatus]
+	ld a, [wEnemyMonStatus] ;original commented line
 	and a
 	ld c, 5
 	jr nz, .addstatus
@@ -381,10 +382,11 @@ PokeBallEffect:
 	; BUG: farcall overwrites a, and GetItemHeldEffect takes b anyway.
 	; This is probably the reason the HELD_CATCH_CHANCE effect is never used.
 	; Uncomment the line below to fix.
+	; UPDATE: Bug has been fixed.
 	ld d, a
 	push de
 	ld a, [wBattleMonItem]
-	; ld b, a
+	ld b, a ;original commented line
 	farcall GetItemHeldEffect
 	ld a, b
 	cp HELD_CATCH_CHANCE
@@ -925,6 +927,7 @@ LureBallMultiplier:
 
 MoonBallMultiplier:
 ; This function is buggy.
+; UPDATE: Bug has been fixed.
 ; Intent:  multiply catch rate by 4 if mon evolves with moon stone
 ; Reality: no boost
 	push bc
@@ -956,7 +959,7 @@ MoonBallMultiplier:
 	push bc
 	ld a, BANK("Evolutions and Attacks")
 	call GetFarByte
-	cp MOON_STONE_RED ; BURN_HEAL
+	cp MOON_STONE ; HAS BEEN CORRECTED
 	pop bc
 	ret nz
 
@@ -971,6 +974,7 @@ MoonBallMultiplier:
 
 LoveBallMultiplier:
 ; This function is buggy.
+; UPDATE: Bug has been fixed.
 ; Intent:  multiply catch rate by 8 if mons are of same species, different sex
 ; Reality: multiply catch rate by 8 if mons are of same species, same sex
 
@@ -1015,7 +1019,7 @@ LoveBallMultiplier:
 	pop de
 	cp d
 	pop bc
-	ret nz ; for the intended effect, this should be "ret z"
+	ret z ; for the intended effect, this should be "ret z"
 
 	sla b
 	jr c, .max
@@ -1036,6 +1040,7 @@ LoveBallMultiplier:
 
 FastBallMultiplier:
 ; This function is buggy.
+; UPDATE: Bug has been fixed.
 ; Intent:  multiply catch rate by 4 if enemy mon is in one of the three
 ;          FleeMons tables.
 ; Reality: multiply catch rate by 4 if enemy mon is one of the first three in
@@ -1053,7 +1058,7 @@ FastBallMultiplier:
 	cp -1
 	jr z, .next
 	cp c
-	jr nz, .next ; for the intended effect, this should be "jr nz, .loop"
+	jr nz, .loop ; for the intended effect, this should be "jr nz, .loop"
 	sla b
 	jr c, .max
 

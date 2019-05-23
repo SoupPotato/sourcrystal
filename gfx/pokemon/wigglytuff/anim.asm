5f9fb48d03b4032bee503058c4e68203db8f90e3
toomanyluigis <toomanyluigis11@gmail.com>
2019-05-23 02:09:00 +0100

    wigglytuff update

--- gfx/pokemon/wigglytuff/anim.asm
+++ gfx/pokemon/wigglytuff/anim.asm
@@ -1,8 +1,16 @@
 	frame 0, 04
-	frame 1, 20
-	frame 2, 08
-	frame 0, 08
+	frame 1, 06
 	frame 2, 06
+	setrepeat 2
+	frame 3, 06
+	frame 4, 06
+	dorepeat 4
+	frame 3, 02
+	frame 2, 02
+	frame 1, 02
+	frame 5, 08
+	frame 0, 08
+	frame 5, 06
 	frame 0, 06
-	frame 2, 04
+	frame 5, 04
 	endanim

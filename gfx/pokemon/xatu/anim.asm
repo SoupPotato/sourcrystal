bacdf172d459f2daca88f7a0655448f6094eacdd
toomanyluigis <toomanyluigis11@gmail.com>
2020-12-12 16:39:54 +0000

    updated Xatu Sprite

--- gfx/pokemon/xatu/anim.asm
+++ gfx/pokemon/xatu/anim.asm
@@ -1,9 +1,10 @@
-	frame 0, 12
-	frame 1, 12
-	frame 0, 06
 	setrepeat 2
 	frame 2, 10
+	frame 4, 03
 	frame 3, 10
-	frame 2, 10
-	dorepeat 5
+	frame 4, 03
+	dorepeat 1
+	frame 2, 06
+	frame 0, 12
+	frame 1, 12
 	endanim

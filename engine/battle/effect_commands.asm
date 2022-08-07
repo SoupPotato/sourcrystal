dbdca8a76bca55798811706c859096fc76d727d0
vulcandth <vulcandth@gmail.com>
2022-08-07 16:13:50 -0500

    Vanilla Bugfix: Reflect and Light Screen can make (Special) Defense wrap around 1024
    This bug existed for all battles in Gold and Silver, and was only fixed for single-player battles in Crystal to preserve link compatibility.

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -2645,10 +2645,6 @@ TruncateHL_BC:
 	inc l
 
 .finish
-; BUG: Reflect and Light Screen can make (Special) Defense wrap around above 1024 (see docs/bugs_and_glitches.md)
-	ld a, [wLinkMode]
-	cp LINK_COLOSSEUM
-	jr z, .done
 ; If we go back to the loop point,
 ; it's the same as doing this exact
 ; same check twice.
@@ -2656,7 +2652,6 @@ TruncateHL_BC:
 	or b
 	jr nz, .loop
 
-.done
 	ld b, l
 	ret
 

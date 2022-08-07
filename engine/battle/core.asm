1ae76da584c9fcd4d7bb9b9ea5f608d704152870
vulcandth <vulcandth@gmail.com>
2022-08-07 16:39:02 -0500

    Vanilla Bugfix: A Disabled by PP Up-enhanced move may not trigger Struggle

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -5791,8 +5791,7 @@ CheckPlayerHasUsableMoves:
 	jr .loop
 
 .done
-; BUG: A Disabled but PP Up–enhanced move may not trigger Struggle (see docs/bugs_and_glitches.md)
-	and a
+	and PP_MASK
 	ret nz
 
 .force_struggle

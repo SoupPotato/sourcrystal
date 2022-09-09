05a4067be9b526c9ceb8c631cc2c87e4be63cf33
vulcandth <vulcandth@gmail.com>
2022-09-08 21:07:55 -0500

    Vanilla Bugfix: AI makes a false assumption about CheckTypeMatchup

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -1399,16 +1399,16 @@ BattleCheckTypeMatchup:
 	ld hl, wEnemyMonType1
 	ldh a, [hBattleTurn]
 	and a
-	jr z, CheckTypeMatchup
+	jr z, .get_type
 	ld hl, wBattleMonType1
+.get_type
+	ld a, BATTLE_VARS_MOVE_TYPE
+	call GetBattleVar ; preserves hl, de, and bc
 	; fallthrough
 CheckTypeMatchup:
-; BUG: AI makes a false assumption about CheckTypeMatchup (see docs/bugs_and_glitches.md)
 	push hl
 	push de
 	push bc
-	ld a, BATTLE_VARS_MOVE_TYPE
-	call GetBattleVar
 	ld d, a
 	ld b, [hl]
 	inc hl

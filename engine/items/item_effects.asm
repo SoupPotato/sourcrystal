9b25a252ddffba6e5414e87f7ac14f378b7c8889
vulcandth <vulcandth@gmail.com>
2022-09-08 20:53:49 -0500

    Vanilla Bugfix: The Dude's catching tutorial may crash if his Poké Ball can't be used
    This can occur if your party and current PC box are both full when you start the tutorial.

--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -210,10 +210,13 @@ ItemEffects:
 ; NoEffect would be appropriate, with the table then being NUM_ITEMS long.
 
 PokeBallEffect:
-; BUG: The Dude's catching tutorial may crash if his Poké Ball can't be used (see docs/bugs_and_glitches.md)
 	ld a, [wBattleMode]
 	dec a
 	jp nz, UseBallInTrainerBattle
+
+	ld a, [wBattleType]
+	cp BATTLETYPE_TUTORIAL
+	jr z, .room_in_party
 
 	ld a, [wPartyCount]
 	cp PARTY_LENGTH

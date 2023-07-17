555a222c8d4024a95df9e99931ed21818d1554c5
vulcandth <vulcandth@gmail.com>
2023-07-17 17:16:54 -0500

    teleport should always succeed in wild battles

--- engine/battle/move_effects/teleport.asm
+++ engine/battle/move_effects/teleport.asm
@@ -15,63 +15,12 @@ BattleCommand_Teleport:
 	call GetBattleVar
 	bit SUBSTATUS_CANT_RUN, a
 	jr nz, .failed
-; Only need to check these next things if it's your turn
-	ld a, [hBattleTurn]
-	and a
-	jr nz, .enemy_turn
+
 ; Switch, don't run, in trainer battles
 	ld a, [wBattleMode]
 	dec a
 	jr nz, .trainer_battle
-
-; If your level is greater than the opponent's, you run without fail.
-	ld a, [wCurPartyLevel]
-	ld b, a
-	ld a, [wBattleMonLevel]
-	cp b
-	jr nc, .run_away
-; Generate a number between 0 and (YourLevel + TheirLevel).
-	add b
-	ld c, a
-	inc c
-.loop_player
-	call BattleRandom
-	cp c
-	jr nc, .loop_player
-; If that number is greater than 4 times your level, run away.
-	srl b
-	srl b
-	cp b
-	jr nc, .run_away
-
-.failed
-	call AnimateFailedMove
-	jp PrintButItFailed
-
-.enemy_turn
-	ld a, [wBattleMode]
-	dec a
-	jp nz, .enemy_trainer
-	ld a, [wBattleMonLevel]
-	ld b, a
-	ld a, [wCurPartyLevel]
-	cp b
-	jr nc, .run_away
-	add b
-	ld c, a
-	inc c
-.loop_enemy
-	call BattleRandom
-	cp c
-	jr nc, .loop_enemy
-	srl b
-	srl b
-	cp b
-	; This does the wrong thing. What was
-	; probably intended was jr c, .failed
-	; The way this is made makes enemy use
-	; of Teleport always succeed if able
-	jr nc, .run_away
+; fallthrough
 .run_away
 	call UpdateBattleMonInParty
 	xor a
@@ -88,6 +37,10 @@ BattleCommand_Teleport:
 
 	ld hl, FledFromBattleText
 	jp StdBattleTextBox
+
+.failed
+	call AnimateFailedMove
+	jp PrintButItFailed
 
 .trainer_battle
 	; Need something to switch to

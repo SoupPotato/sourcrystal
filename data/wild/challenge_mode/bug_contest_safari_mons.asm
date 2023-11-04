47ecd89a304a554448be5766f6422c99baa5b045
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2023-11-04 07:00:19 +0700

    buff up bug contest mons in challenge mode

--- data/wild/challenge_mode/bug_contest_safari_mons.asm
+++ data/wild/challenge_mode/bug_contest_safari_mons.asm
@@ -2,41 +2,41 @@ ContestSafariMonsChallenge:
 	;   %, species,   min, max
 
 	map_id NATIONAL_PARK_BUG_CONTEST
-.NationalPark ; TODO: change this?
+.NationalPark
 	table_width 4, .NationalPark
 ; morning
-	db 20, CATERPIE,    7, 18
-	db 20, WEEDLE,      7, 18
-	db 10, METAPOD,     9, 18
-	db 10, KAKUNA,      9, 18
-	db  5, BUTTERFREE, 12, 15
-	db  5, BEEDRILL,   12, 15
-	db 10, VENONAT,    10, 16
-	db 10, PARAS,      10, 17
-	db  5, SCYTHER,    13, 14
-	db  5, PINSIR,     13, 14
+	db 20, CATERPIE,    9, 20
+	db 20, WEEDLE,      9, 20
+	db 10, METAPOD,    11, 20
+	db 10, KAKUNA,     11, 20
+	db  5, BUTTERFREE, 14, 17
+	db  5, BEEDRILL,   14, 17
+	db 10, VENONAT,    12, 18
+	db 10, PARAS,      12, 19
+	db  5, SCYTHER,    15, 16
+	db  5, PINSIR,     15, 16
 ; day/eve
-	db 20, CATERPIE,    7, 18
-	db 20, WEEDLE,      7, 18
-	db 10, METAPOD,     9, 18
-	db 10, KAKUNA,      9, 18
-	db  5, BUTTERFREE, 12, 15
-	db  5, BEEDRILL,   12, 15
-	db 10, VENONAT,    10, 16
-	db 10, PARAS,      10, 17
-	db  5, SCYTHER,    13, 14
-	db  5, PINSIR,     13, 14
+	db 20, CATERPIE,    9, 20
+	db 20, WEEDLE,      9, 20
+	db 10, METAPOD,    11, 20
+	db 10, KAKUNA,     11, 20
+	db  5, BUTTERFREE, 14, 17
+	db  5, BEEDRILL,   14, 17
+	db 10, VENONAT,    12, 18
+	db 10, PARAS,      12, 19
+	db  5, SCYTHER,    15, 16
+	db  5, PINSIR,     15, 16
 ; nite
-	db 20, CATERPIE,    7, 18
-	db 20, WEEDLE,      7, 18
-	db 10, METAPOD,     9, 18
-	db 10, KAKUNA,      9, 18
-	db  5, BUTTERFREE, 12, 15
-	db  5, BEEDRILL,   12, 15
-	db 10, VENONAT,    10, 16
-	db 10, PARAS,      10, 17
-	db  5, SCYTHER,    13, 14
-	db  5, PINSIR,     13, 14
+	db 20, CATERPIE,    9, 20
+	db 20, WEEDLE,      9, 20
+	db 10, METAPOD,    11, 20
+	db 10, KAKUNA,     11, 20
+	db  5, BUTTERFREE, 14, 17
+	db  5, BEEDRILL,   14, 17
+	db 10, VENONAT,    12, 18
+	db 10, PARAS,      12, 19
+	db  5, SCYTHER,    15, 16
+	db  5, PINSIR,     15, 16
 	assert_table_length 30
 	
 	map_id SAFARI_ZONE_AREA_1

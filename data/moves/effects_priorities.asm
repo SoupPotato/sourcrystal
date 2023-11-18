2f713ac088500fdefe35eb85661f78db77d8b83d
SoupPotato <toomanyluigis11@gmail.com>
2023-11-18 00:53:29 +0000

    Bide now has increased priority

--- data/moves/effects_priorities.asm
+++ data/moves/effects_priorities.asm
@@ -2,6 +2,7 @@ MoveEffectPriorities:
 	db EFFECT_PROTECT,      3
 	db EFFECT_ENDURE,       3
 	db EFFECT_PRIORITY_HIT, 2
+	db EFFECT_BIDE,         2
 	db EFFECT_FORCE_SWITCH, 0
 	db EFFECT_COUNTER,      0
 	db EFFECT_MIRROR_COAT,  0

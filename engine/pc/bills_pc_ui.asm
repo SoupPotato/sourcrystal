238e7466985e7b5c74d9df01a6000a77dace2ace
SoupPotato <toomanyluigis11@gmail.com>
2023-11-25 04:23:15 +0000

    Unowns now display correctly in PC Box

--- engine/pc/bills_pc_ui.asm
+++ engine/pc/bills_pc_ui.asm
@@ -1003,6 +1003,8 @@ _GetCursorMon:
 	; Prepare frontpic. Split into decompression + loading to make sure we
 	; refresh the pokepic and the palette in a single frame (decompression
 	; is unpredictable, but bpp copy can be relied upon).
+	ld hl, wBufferMonDVs
+	predef GetUnownLetter
 	ld a, [wBufferMonAltSpecies]
 	ld [wCurPartySpecies], a
 	ld [wCurSpecies], a

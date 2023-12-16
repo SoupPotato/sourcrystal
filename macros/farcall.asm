9227fdee93a33065a685bbe885b886f8597feee4
SoupPotato <toomanyluigis11@gmail.com>
2023-12-16 21:06:45 +0000

    Revert "chore: Add assert warn for unnecessary farcalls"
    This reverts commit a3227f7b1508dbe720ba4a5986a05a114ffd45e0.

--- macros/farcall.asm
+++ macros/farcall.asm
@@ -1,34 +1,26 @@
 ; Far calls to another bank
 
 MACRO farcall ; bank, address
-	assert !(\1 & $8000), "cannot `farcall \1` in RAM"
 	ld a, BANK(\1)
 	ld hl, \1
 	rst FarCall
-	assert warn, BANK(\1) != 0 && BANK(\1) != BANK(@), "unnecessary `farcall \1`"
 ENDM
 
 MACRO newfarcall ; bank, address
-	assert !(\1 & $8000), "cannot `newfarcall \1` in RAM"
 	; We can't replace previous farcalls because it breaks saves...
 	rst BetterFarCall
 	dbw BANK(\1), \1
-	assert warn, BANK(\1) != 0 && BANK(\1) != BANK(@), "unnecessary `newfarcall \1`"
 ENDM
 
 MACRO newfarjp
-	assert !(\1 & $8000), "cannot `newfarjp \1` in RAM"
 	rst BetterFarCall
 	dbw BANK(\1) | $80, \1
-	assert warn, BANK(\1) != 0 && BANK(\1) != BANK(@), "unnecessary `newfarjp \1`"
 ENDM
 
 MACRO callfar ; address, bank
-	assert !(\1 & $8000), "cannot `callfar \1` in RAM"
 	ld hl, \1
 	ld a, BANK(\1)
 	rst FarCall
-	assert warn, BANK(\1) != 0 && BANK(\1) != BANK(@), "unnecessary `callfar \1`"
 ENDM
 
 MACRO homecall
@@ -39,5 +31,4 @@ MACRO homecall
 	call \1
 	pop af
 	rst Bankswitch
-	assert warn, BANK(\1) != 0, "unnecessary `homecall \1`"
 ENDM

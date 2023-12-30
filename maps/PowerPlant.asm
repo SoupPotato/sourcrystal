dd354c12277f7e8e661c39ab781b4d5d1aa0c6d4
SoupPotato <toomanyluigis11@gmail.com>
2023-12-30 13:40:18 +0000

    Bugfix - Power Plant has incorrect Warp

--- maps/PowerPlant.asm
+++ maps/PowerPlant.asm
@@ -390,8 +390,8 @@ PowerPlant_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  2, 17, ROUTE_10_NORTH, 2
-	warp_event  3, 17, ROUTE_10_NORTH, 2
+	warp_event  2, 17, ROUTE_10_SOUTH, 1
+	warp_event  3, 17, ROUTE_10_SOUTH, 1
 
 	def_coord_events
 	coord_event  5, 12, SCENE_POWERPLANT_GUARD_GETS_PHONE_CALL, PowerPlantGuardPhoneScript

fellowship-of-the-roms/CrystalShireEngine

Some Dynamic Pal Improvements

engine/menus/start_menu.asm
@@ -139,10 +139,8 @@ StartMenu::
 	jr .ReturnEnd2
 
 .ReturnRedraw:
-	call .Clear
-	jmp .Reopen
-
-.Clear:
+	farcall ClearSavedObjPals
+	farcall DisableDynPalUpdates
 	call ClearBGPalettes
 	call ExitMenu
 	call ReloadTilesetAndPalettes
@@ -150,7 +148,8 @@ StartMenu::
 	call DrawVariableLengthMenuBox
 	call .DrawBugContestStatus
 	call UpdateSprites
-	jmp FinishExitMenu
+	call FinishExitMenu
+	jmp .Reopen
 
 .MenuHeader:
 	db MENU_BACKUP_TILES ; flags

SoupPotato/sourcrystal

engine/menus/start_menu.asm
@@ -143,10 +143,8 @@ StartMenu::
 	jr .ReturnEnd2
 
 .ReturnRedraw:
-	call .Clear
-	jp .Reopen
-
-.Clear:
+	farcall ClearSavedObjPals
+	farcall DisableDynPalUpdates
 	call ClearBGPalettes
 	call Call_ExitMenu
 	call ReloadTilesetAndPalettes
@@ -157,7 +155,7 @@ StartMenu::
 	call UpdateSprites
 	call GSReloadPalettes
 	call FinishExitMenu
-	ret
+	jp .Reopen
 
 .MenuHeader:
 	db MENU_BACKUP_TILES ; flags
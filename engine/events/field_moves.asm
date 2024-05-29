fellowship-of-the-roms/CrystalShireEngine

Various Dynamic Pal Fixes
* Fix TimeOfDay pal transitions
* Fix DynPal comap with pal fade
* Fix DynPal Flash in Darkness and FadeInPalettes
* Fix for FastShip CaptainsCabin scene transition

resolves #18

engine/events/field_moves.asm
@@ -17,7 +17,7 @@ BlindingFlash:
 	ld b, SCGB_MAPPALS
 	call GetSGBLayout
 	farcall LoadOW_BGPal7
-	jmp FadeInPalettes
+	jmp FadeInPalettes_EnableDynNoApply
 
 ShakeHeadbuttTree:
 	farcall CopyBGGreenToOBPal7

home/palettes.asm
@@ -312,3 +312,18 @@ ReloadSpritesNoPalettes::
 	ld a, TRUE
 	ldh [hCGBPalUpdate], a
 	jmp DelayFrame
+
+SetBlackObjectPals::
+	ldh a, [rSVBK]
+	push af
+	ld a, BANK(wOBPals2)
+	ldh [rSVBK], a
+	ld hl, wOBPals2
+	ld bc, 8 palettes
+	xor a
+	rst ByteFill
+	pop af
+	ldh [rSVBK], a
+	ld a, 1
+	ldh [hCGBPalUpdate], a
+	jmp DelayFrame

SoupPotato/sourcrystal

engine/events/field_moves.asm
@@ -18,7 +18,7 @@ BlindingFlash:
 	ld b, SCGB_MAPPALS
 	call GetSGBLayout
 	farcall LoadOW_BGPal7
-	farcall FadeInPalettes
+	farcall FadeInPalettes_EnableDynNoApply
 	ret
 
 ShakeHeadbuttTree:

home/palettes.asm
@@ -331,3 +331,18 @@ SwapTextboxPalettes::
 ScrollBGMapPalettes::
 	homecall _ScrollBGMapPalettes
 	ret
+
+SetBlackObjectPals::
+	ldh a, [rSVBK]
+	push af
+	ld a, BANK(wOBPals2)
+	ldh [rSVBK], a
+	ld hl, wOBPals2
+	ld bc, 8 palettes
+	xor a
+	call ByteFill
+	pop af
+	ldh [rSVBK], a
+	ld a, 1
+	ldh [hCGBPalUpdate], a
+	jp DelayFrame
f2ac2d62ec379310ce5704b3b959e86a57229b3c
SoupPotato <toomanyluigis11@gmail.com>
2024-12-16 05:44:38 +0000

    Updated Egg rates to gen3+

--- engine/events/happiness_egg.asm
+++ engine/events/happiness_egg.asm
@@ -193,25 +193,24 @@ DayCareStep::
 	dec [hl]
 	ret nz
 
-	call Random
-	ld [hl], a
-	callfar CheckBreedmonCompatibility
+	farcall CheckBreedmonCompatibility
 	ld a, [wBreedingCompatibility]
-	cp 230
-	ld b, 31 percent + 1
-	jr nc, .okay
-	ld a, [wBreedingCompatibility]
-	cp 170
-	ld b, 16 percent
-	jr nc, .okay
-	ld a, [wBreedingCompatibility]
-	cp 110
-	ld b, 12 percent
-	jr nc, .okay
-	ld b, 4 percent
+	; Egg initialization shouldn't happen if incompatible, but just in case
+	and a
+	ret z
+
+	dec a ; 1: Semi-compatible
+	lb bc, 20, 40
+	jr z, .okay
+	dec a ; 2: Compatible
+	lb bc, 50, 80
+	jr z, .okay
+	; 3: Very compatible
+	lb bc, 70, 88
 
 .okay
-	call Random
+	ld a, 100
+	call RandomRange
 	cp b
 	ret nc
 	ld hl, wDayCareMan

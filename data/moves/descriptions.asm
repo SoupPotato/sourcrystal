8131d036b284e74f18f163d4ec60d1de20c48fa9
SoupPotato <toomanyluigis11@gmail.com>
2025-10-08 14:19:24 +0100

    Corrected Label

--- data/moves/descriptions.asm
+++ data/moves/descriptions.asm
@@ -185,7 +185,7 @@ MoveDescriptions::
 	dw ProtectDescription
 	dw MachPunchDescription
 	dw ScaryFaceDescription
-	dw FaintAttackDescription
+	dw FeintAttackDescription
 	dw SweetKissDescription
 	dw BellyDrumDescription
 	dw SludgeBombDescription
@@ -1003,7 +1003,7 @@ ScaryFaceDescription:
 	db   "Sharply reduces"
 	next "the foe's SPEED.@"
 
-FaintAttackDescription:
+FeintAttackDescription:
 	db   "An attack that"
 	next "never misses.@"
 

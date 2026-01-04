4199a7435431d4c27e8fa4294656b1009b754963
SoupPotato <toomanyluigis11@gmail.com>
2026-01-04 10:56:25 +0000

    Updated Version number

--- engine/menus/main_menu.asm
+++ engine/menus/main_menu.asm
@@ -260,25 +260,25 @@ MainMenuJoypadLoop:
 	ret
 
 MainMenu_PrintCurrentVersion:
-	lb bc, 1, 4 ;Box - Width
+	lb bc, 1, 5 ;Box - Width
 	ld a, [wSaveFileExists]
 	and a
 	jr nz, .has_save_file
 ; no save file
-	hlcoord 14, 15 ;Box - Position
+	hlcoord 13, 15 ;Box - Position
 	call Textbox
-	hlcoord 15, 16 ;Text - Width
+	hlcoord 14, 16 ;Text - Width
 	jr .no_save_file
 .has_save_file
-	hlcoord 14, 11 ;Box - Position
+	hlcoord 13, 11 ;Box - Position
 	call Textbox
-	hlcoord 15, 12 ;Text - Width
+	hlcoord 14, 12 ;Text - Width
 .no_save_file
 	ld de, .VersionString
 	jp PlaceString
 
 .VersionString
-	db "v6.0"
+	db "v6.0a"
 	db "@"
 
 MainMenu_PrintCurrentTimeAndDay:

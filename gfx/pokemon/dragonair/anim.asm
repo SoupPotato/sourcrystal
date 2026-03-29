4e48dbbb7a05d500c2c82c0f35c0f1a564accada
SoupPotato <toomanyluigis11@gmail.com>
2026-03-29 21:00:47 +0100

    Updated Dragonair Sprite

--- gfx/pokemon/dragonair/anim.asm
+++ gfx/pokemon/dragonair/anim.asm
@@ -1,7 +1,10 @@
 	setrepeat 2
-	frame 1, 08
-	frame 2, 08
-	frame 3, 08
-	frame 2, 08
+	frame 1, 09
+	frame 2, 09
+	frame 3, 09
 	dorepeat 1
+	frame 0, 08
+	frame 5, 08
+	frame 6, 08
+	frame 7, 08
 	endanim
